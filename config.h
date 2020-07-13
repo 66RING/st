0dc4767329da9b182a94c6f46548c6c033b81524
66RING <1552087953@qq.com>
2020-07-13 21:37:00 +0800

    update some

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static char *font2[] = {
 	"Source Han Serif CN:pixelsize=24:antialias=true:autohint=true",
 };
 
-static int borderpx = 8;
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -93,7 +93,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
